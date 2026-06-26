3816d7ea2ddfbe62be3dc2d6801892b564a032d5
JiraphatD <jiraphat.deaw@gmail.com>
2026-06-26 14:40:10 +0700

    add

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -12,7 +12,7 @@ func (s *FiberServer) RegisterFiberRoutes() {
 }
 
 func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
-	key := "Jiraphat2545"
+	key := "oeUtXt5pwFyR1W2uetBzKYd3v8vwdlFH"
 	resp := fiber.Map{
 		"message": key,
 	}
